8a9dc673ad04ebcaf0d4949e008e4904dd7c2a0c
utpal <utpalvir98@gmail.com>
2024-04-22 00:11:34 +0530

    finalpush

--- WebSCrapper/WebScrapping/models.py
+++ WebSCrapper/WebScrapping/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Link(models.Model):
+    
+    address = models.CharField(max_length=100,blank=True,null=True)
+    name = models.CharField(max_length=100, null=True,blank=True)
+    def __str__(self):
+        return self.name or ' '
+    
--- WebSCrapper/WebScrapping/views.py
+++ WebSCrapper/WebScrapping/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
+from django.http import HttpResponseRedirect
+from .models import *
+
 
 # Create your views here.
+
+
+def scrape(request):
+    if request.method=="POST":
+        site=request.POST.get('site','')
+        page = requests.get(site)
+        soup = BeautifulSoup(page.text , 'html.parser')
+        
+        
+        for link in soup.find_all('a'):
+            link_address = link.get('href')
+            link_text = link.string
+            Link.objects.create(address = link_address, name =link_text)
+        return HttpResponseRedirect('/')
+    else:
+        data = Link.objects.all()
+        print(data)
+    return render(request,'myapp/result.html', {'data':data})
+def clear(request):
+    Link.objects.all().delete()
+    return render(request,'myapp/result.html')
